68f3a2bf2efafe42af9ffb4f5a8ee6676a7f63a1
whdgnszz1 <whdgnszz1@snu.ac.kr>
2024-03-18 16:41:02 +0900

    Separate auto config file using DeferredImportSelector

--- src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
+++ src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,31 @@
 package tobyspring.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "tobyspring.config.autoconfig.DispatcherServletConfig",
-                "tobyspring.config.autoconfig.TomcatWebServerConfig",
-        };
+        List<String> autoConfigs = new ArrayList<>();
+
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+
+        return autoConfigs.toArray(new String[0]);
+//        return Arrays.copyOf(autoConfigs.toArray(), autoConfigs.size(), String[].class);
+
+//        return autoConfigs.stream().toArray(String[]::new);
+
+//        Iterable<String> candidates = ImportCandidates.load(MyAutoConfigImportSelector.class, classLoader);
+//        return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
     }
 }
